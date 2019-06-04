3ddfff26728f66ca8adfe46cbcf6cab1548951ef
Charlie Thomson <charliecthomson@gmail.com>
2019-06-03 20:12:52 -0400

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,228 @@
-/*
-IDEA:
-    `token`: any item in an expression; can be a number, function, or operator
-    `expression`: wrapper for Vec<Token>; works on tokens in bulk
-
-*/
-
 use std::{
-    alloc::{ alloc_zeroed, dealloc, realloc },
-
+    io::{ Error, ErrorKind, },
+    fmt::{ Debug, Display, Formatter, Result as fmt_Result, },
 };
 
-struct Token {
-    value: String,
-} impl Token {
-    fn new() -> Token {
-        Token {
-            value: String::new()
+#[derive(Clone)]
+enum Paren {
+    Left,
+    Right,
+} impl Paren {
+
+    fn is_left(&self) -> bool {
+        match self {
+            Paren::Left => true,
+            _ => false,
+        }
+    }
+
+    fn is_right(&self) -> bool {
+        match self {
+            Paren::Right => true,
+            _ => false,
         }
     }
     
-    fn from(s: String) -> Token {
-        Token {
-            value: s,
+}
+
+#[derive(Clone)]
+enum Operator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Paren(Paren),
+} impl Operator {
+    fn char_is_valid(c: &char) -> bool {
+        match Self::from_char(c) {
+            Ok(_) => true,
+            _     => false,
         }
     }
 
-    fn has_value(&self) -> bool {
-        !self.value.is_empty()
+    fn from_char(c: &char) -> Result<Self, Error> {
+        match c {
+            '+' => Ok(Operator::Add),
+            '-' => Ok(Operator::Sub),
+            '/' => Ok(Operator::Mul),
+            '*' => Ok(Operator::Div),
+            '%' => Ok(Operator::Mod),
+            '(' => Ok(Operator::Paren(Paren::Left)),
+            ')' => Ok(Operator::Paren(Paren::Right)),
+            o   => Err(Error::new(ErrorKind::Other, format!("Unexpected operator in <Operator>::from_char({})", o)))
+        }
     }
 
-    fn as_string(&self) -> String {
+    fn as_str(&self) -> String {
+        match self {
+
+        
+            Operator::Add => format!("{}", '+'),
+            Operator::Sub => format!("{}", '-'),
+            Operator::Mul => format!("{}", '/'),
+            Operator::Div => format!("{}", '*'),
+            Operator::Mod => format!("{}", '%'),
+            Operator::Paren(Paren::Left) => format!("{}", '('),
+            Operator::Paren(Paren::Right) => format!("{}", ')'),
+        }
+    }
+
+}
+
+#[derive(Clone)]
+struct Number {
+    value: usize,    
+} impl Number
+{
+    fn new() -> Self {
+        Self {
+            value: 0,
+        }
+    }
+
+    fn with_value(value: usize) -> Self {
+        Self {
+            value,
+        }
+    }
+
+    fn get_value(&self) -> usize {
         self.value.clone()
     }
 
-    fn as_char(&self) -> char {
-        self.value.chars().nth(0).unwrap()
+    fn set_value(&mut self, v: usize) {
+        self.value = v;
     }
 
-    fn as_bytes(&self) -> &[u8] {
-        self.value.as_bytes()
+    fn as_str(&self) -> String {
+        format!("{}", self.value)
+    }
+}
+
+#[derive(Clone)]
+enum Token {
+    Number(Number),
+    Operator(Operator),
+    Whitespace,
+} impl Token {
+    
+    fn whitespace() -> Self {
+        Token::Whitespace
     }
 
-    fn is_numeric(&self) -> bool {
-        for c in self.value.chars() {
-            if !c.is_numeric() { return false; }
+    fn number(num: usize) -> Self {
+        Token::Number(Number::with_value(num))
+    }
+
+    fn number_from_str(s: String) -> Result<Self, Error> {
+        match s.parse::<usize>() {
+            Ok(a) => Ok(Token::number(a)),
+            Err(e) => Err(Error::new(ErrorKind::Other, format!("<Token>::number_from_str({}) error: {}", s, e)))
         }
-        true
+    }
+
+    fn is_number(&self) -> bool {
+        match self {
+            Token::Number(_) => true,
+            _ => false,
+        }
+    }
+
+    fn operator(op: Operator) -> Self {
+        Token::Operator(op)
+    }
+
+    fn operator_from_char(c: &char) -> Result<Self, Error> {
+        let opr = Operator::from_char(c)?;
+        Ok(Token::operator(opr))
     }
 
     fn is_operator(&self) -> bool {
-        ['(', ')', '+', '-', '*', '/', '^'].contains(&self.as_char())
+        match self {
+            Token::Operator(_) => true,
+            _ => false,
+        }
+    }
+    
+    fn as_str(&self) -> String {
+        match self {
+            Token::Whitespace => format!("{}", ' '),
+            Token::Number(num) => num.as_str(),
+            Token::Operator(op) => op.as_str(),
+        }
+    }
+}
+
+struct TokenStream {
+    tokens: Vec<Token>,
+} impl TokenStream {
+    fn new() -> Self {
+        Self { tokens: Vec::new() }
     }
 
-    fn is_paren(&self) -> bool {
-        ['(', ')'].contains(&self.as_char())
+    fn from_string(s: &String) -> Result<Self, Error> {
+        let mut iter = s.chars();
+        let mut buf = String::new();
+        let mut stream = Self::new();
+
+        while let Some(c) = iter.next() {
+            // Skip whitespace
+            if c.is_whitespace() {
+                continue;
+            } else if c.is_numeric() {
+                // add the number to the buffer
+                buf.push(c);
+                // search for more digits
+                'num_search: loop {
+                    match iter.next() {
+                        // if we find a number, add it to the buffer, and continue searching
+                        Some(x) if x.is_numeric() => {
+                            buf.push(x);
+                        },
+                        // if we find a different character (an operator), add the number to the stream,
+                        // clear the buffer, and add the operator to the stream
+                        Some(x) => {
+                            if x.is_whitespace() { continue; }
+                            // if the character is not an operator, return an error
+                            else if !Operator::char_is_valid(&x) {
+                                return Err(Error::new(ErrorKind::Other, format!("Encountered unexpected character: '{}'", x)));
+                            }
+                            stream.add_token(Token::number_from_str(buf.clone())?);
+                            buf = String::new();
+                            stream.add_token(Token::operator_from_char(&x)?);
+                            break 'num_search;
+                        },
+                        // otherwise, break the search (will exit the main loop, no chars left in the iterator)
+                        _ => break 'num_search
+                    }
+                }
+            // if the character is an operator
+            } else if Operator::char_is_valid(&c) {
+                stream.add_token(Token::operator_from_char(&c)?);
+                buf = String::new();
+            }
+        }
+
+        return Ok(stream);
     }
 
+    fn add_token(&mut self, token: Token) {
+        self.tokens.push(token);
+    }
+} impl Debug for TokenStream {
+    fn fmt(&self, f: &mut Formatter) -> fmt_Result {
+        for token in self.tokens.clone().iter() {
+            write!(f, "{} ", token.as_str());
+        }
+        write!(f, "")
+    }
+}
 
+
+
+fn main() {
+    let s = String::from("(10 + 11) - 3 / (4 * 11)");
+
+    eprintln!("original: {};\nTokenStream: {:?};", &s, TokenStream::from_string(&s).unwrap());
 }
