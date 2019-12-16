53513f831bd9d6a438778701ff096128b5f5558e
Charlie Thomson <charliecthomson@gmail.com>
2019-12-16 13:20:42 -0500

    step

--- pemdas-eval/src/token.rs
+++ pemdas-eval/src/token.rs
@@ -1,35 +1,362 @@
 
-use crate::tree::{ Operator, Paren };
+use std::{
+    str::FromStr,
+    fmt::{ Debug, Display, Formatter, Result as fmt_Result }
+};
 
-#[derive(Clone, Copy, PartialEq)]
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum Operator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow
+} impl Operator {
+    pub fn from_char(c: char) -> Option<Self> {
+        match c {
+            '+' => Some(Self::Add),
+            '-' => Some(Self::Sub),
+            '*' => Some(Self::Mul),
+            '/' => Some(Self::Div),
+            '^' => Some(Self::Pow),
+            _ => None,
+        }
+    }
+
+    pub fn to_char(&self) -> char {
+        match self {
+            Self::Add => '+',
+            Self::Sub => '-',
+            Self::Mul => '*',
+            Self::Div => '/',
+            Self::Pow => '^',
+        }
+    }
+
+    pub fn evaluate(&self, left: f64, right: f64) -> f64 {
+        match self {
+            Self::Add => left + right,
+            Self::Sub => left - right,
+            Self::Mul => left * right,
+            Self::Div => left / right,
+            Self::Pow => left.powf(right),
+        }
+    }
+} impl FromStr for Operator {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Operator, Self::Err> {
+        match Operator::from_char(s
+                                .chars()
+                                .next()
+                                .expect("Cannot parse empty string into operator")
+                            ) {
+            Some(n) => Ok(n),
+            None => Err("Unknown operator"),
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub enum Paren {
+    Left,
+    Right,
+} impl Paren {
+    pub fn from_char(c: char) -> Option<Self> {
+        match c {
+            '(' => Some(Self::Left),
+            ')' => Some(Self::Right),
+            _ => None,
+        }
+    }
+
+    pub fn to_char(&self) -> char {
+        match self {
+            Self::Left => '(',
+            Self::Right => ')',
+        }
+    }
+} impl FromStr for Paren {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Paren, Self::Err> {
+        match Paren::from_char(s
+                                .chars()
+                                .next()
+                                .expect("Cannot parse empty string into paren")
+                            ) {
+            Some(n) => Ok(n),
+            None => Err("Unknown literal"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub enum Token {
     Operator(Operator),
     Value(f64),
     Paren(Paren),
 } impl Token {
     pub fn new(literal: &str) -> Self {
-        if let Ok(v) = literal.parse::<f64>() {
-            Token::Value(v)
-        } else if let Some(op) = Operator::from_char(literal
-                                                        .chars()
-                                                        .next()
-                                                        .expect("Cannot create node from empty literal")) {
-            Token::Operator(op)
-        } else {
-            panic!("Unexpected literal: '{:?}', failed classification", literal);
+        match literal.parse::<Token>() {
+            Ok(t) => t,
+            Err(e) => panic!(e)
         }
+    }
+} impl FromStr for Token {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Token, Self::Err> {
+        if let Ok(v) = s.parse::<f64>() {
+            Ok(Token::Value(v))
+        } else if let Ok(op) = s.parse::<Operator>() {
+            Ok(Token::Operator(op))
+        } else if let Ok(p) = s.parse::<Paren>() {
+            Ok(Token::Paren(p))
+        } else {
+            Err("Unexpected literal")
+        }
+    } 
+} impl Display for Token {
+    fn fmt(&self, f: &mut Formatter) -> fmt_Result {
+        write!(f, "{}", {
+            match self {
+                Token::Operator(op) => format!("{}", op.to_char()),
+                Token::Paren(p) => format!("{}", p.to_char()),
+                Token::Value(v) => format!("{}", v)
+            }
+        })
     }
 }
 
+/// Parse the string `s` into a Token stream
+/// ```rust
+/// let tokens = vec![
+///     Token::new("("),
+///     Token::new("10"),
+///     Token::new("+"),
+///     Token::new("5"),
+///     Token::new(")"),
+/// ];
+/// assert!(tokens == tokenize("(10+5)"));
+/// ```
 pub fn tokenize(s: &str) -> Vec<Token> {
     let mut buffer = String::new();
     let mut tokens: Vec<Token> = Vec::new();
 
+    let cleaned = s.chars()
+                .filter(|&c| ".0123456789/*-+^()".contains(c))
+                .collect::<String>();
+
+    let mut idx = 0;
+
+    while let Some(c) = cleaned.chars().nth(idx) {
+        if c.is_numeric() || c == '.' {
+            buffer.push(c);
+        } else if !buffer.is_empty() {
+            tokens.push(buffer
+                    .parse()
+                    .expect("Failed to parse token from buffer")
+                );
+            buffer = String::new();
+            idx -= 1;
+        } else if let Ok(token) = c.to_string().parse() {
+            tokens.push(token);
+        } else {
+            panic!("Encountered unexpected character '{}'", c);
+        }
+
+        idx += 1;
+    }
     
+    if !buffer.is_empty() {
+        tokens.push(buffer
+            .parse()
+            .expect("Failed to parse token from buffer")
+        );
+    }
 
     tokens
 }
 
+#[derive(Clone, Copy, PartialEq)]
+enum OpWrapper {
+    Operator(Operator),
+    Paren(Paren),
+} impl OpWrapper {
+    fn to_token(self) -> Token {
+        match self {
+            OpWrapper::Operator(op) => Token::Operator(op),
+            OpWrapper::Paren(p) => Token::Paren(p),
+        }
+    }
+} impl From<Operator> for OpWrapper {
+    fn from(op: Operator) -> Self {
+        OpWrapper::Operator(op)
+    }
+} impl From<Paren> for OpWrapper {
+    fn from(p: Paren) -> Self {
+        OpWrapper::Paren(p)
+    }
+}
+
+
+fn precedence(op: &OpWrapper) -> u32 {
+    match op {
+        OpWrapper::Operator(o) => {
+            match o {
+                Operator::Add => 2,
+                Operator::Sub => 2,
+                Operator::Mul => 3,
+                Operator::Div => 3,
+                Operator::Pow => 4,
+            }
+        }, 
+        _ => 0
+        
+    }
+}
+
+#[derive(Copy, Clone, PartialEq)]
+enum OperatorAssociativity {
+    Left, Right
+} impl From<OpWrapper> for OperatorAssociativity {
+    fn from(op: OpWrapper) -> Self {
+        match op {
+            OpWrapper::Operator(Operator::Pow) => OperatorAssociativity::Right,
+            _ => OperatorAssociativity::Left,
+        }
+    }
+} impl<T> From<&T> for OperatorAssociativity where T: Clone, OperatorAssociativity: From<T> {
+    fn from(v: &T) -> Self {
+        Self::from(v.clone())
+    }
+}
+
 pub fn shunting_yard(tokens: Vec<Token>) -> Vec<Token> {
-    Vec::new()
+    let mut output: Vec<Token> = Vec::new();
+    let mut opstack: Vec<OpWrapper> = Vec::new();
+    
+    for token in tokens {
+        match token {
+            Token::Value(_) => {
+                output.push(token);
+            },
+            Token::Operator(op) => {
+                while 
+                    !opstack.is_empty() 
+                    &&
+                    (
+                        precedence(&OpWrapper::from(op)) < precedence(opstack.last().unwrap())
+                        ||
+                        (
+                            precedence(&OpWrapper::from(op)) == precedence(opstack.last().unwrap()) 
+                            && 
+                            OperatorAssociativity::from(opstack.last().unwrap()) == OperatorAssociativity::Left
+                        )
+                        &&
+                        (opstack.last().unwrap() != &OpWrapper::Paren(Paren::Left)
+                    )
+                ) {
+                        output.push(opstack.pop().unwrap().to_token())
+                }
+
+                opstack.push(OpWrapper::from(op));
+            },
+            Token::Paren(p) => {
+                match p {
+                    Paren::Left => opstack.push(OpWrapper::from(p)),
+                    Paren::Right => {
+                        while opstack.last().expect("Mismatched parens") != &OpWrapper::Paren(Paren::Left) {
+                            output.push(opstack.pop().unwrap().to_token());
+                        }
+
+                        opstack.pop();
+                    }
+                };
+
+            },
+        }
+    }
+
+    while !opstack.is_empty() {
+        match opstack.pop().unwrap() {
+            OpWrapper::Paren(_) => {
+                panic!("Mismatched parens!");
+            },
+            a => {
+                output.push(a.to_token())
+            }
+        }
+    }
+
+    output
+}
+
+
+
+#[test]
+fn test_tokenize() {
+    // Basic
+    let tokens = vec![
+        Token::new("("),
+        Token::new("10"),
+        Token::new("+"),
+        Token::new("5"),
+        Token::new(")"),
+    ];
+    assert!(tokens == tokenize("(10+5)"));
+
+    // Complex
+    let tokens = vec![
+        Token::new("("),
+        Token::new("("),
+        Token::new("10.0"),
+        Token::new("*"),
+        Token::new("2"),
+        Token::new(")"),
+        Token::new("/"),
+        Token::new("4"),
+        Token::new("+"),
+        Token::new("("),
+        Token::new("2.5"),
+        Token::new("*"),
+        Token::new("4"),
+        Token::new(")"),
+        Token::new("*"),
+        Token::new("2"),
+        Token::new(")"),
+    ];
+    assert!(tokens == tokenize("((10.0 * 2) / 4 + (2.5 * 4) * 2)"));
+
+    // No parens
+    let tokens = vec![
+        Token::new("10"),
+        Token::new("+"),
+        Token::new("5"),
+    ];
+    assert!(tokens == tokenize("10.0 + 5"));
+}   
+
+#[test]
+fn test_shunting_yard() {
+    let tokens = tokenize("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3");
+    let expected = vec![
+        Token::new("3"),
+        Token::new("4"),
+        Token::new("2"),
+        Token::new("*"),
+        Token::new("1"),
+        Token::new("5"),
+        Token::new("-"),
+        Token::new("2"),
+        Token::new("3"),
+        Token::new("^"),
+        Token::new("^"),
+        Token::new("/"),
+        Token::new("+"),
+    ];
+    assert_eq!(shunting_yard(tokens), expected);
+
 }
--- pemdas-eval/src/tree.rs
+++ pemdas-eval/src/tree.rs
@@ -1,81 +1,23 @@
 
-use crate::token::Token;
+use crate::token::{ Token, Operator, Paren};
+use std::str::FromStr;
 
-#[derive(Clone, Copy, PartialEq)]
-pub enum Operator {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Pow
-} impl Operator {
-    pub fn from_char(c: char) -> Option<Self> {
-        match c {
-            '+' => Some(Self::Add),
-            '-' => Some(Self::Sub),
-            '*' => Some(Self::Mul),
-            '/' => Some(Self::Div),
-            '^' => Some(Self::Pow),
-            _ => None,
-        }
-    }
 
-    pub fn to_char(&self) -> char {
-        match self {
-            Self::Add => '+',
-            Self::Sub => '-',
-            Self::Mul => '*',
-            Self::Div => '/',
-            Self::Pow => '^',
-        }
-    }
-
-    pub fn evaluate(&self, left: f64, right: f64) -> f64 {
-        match self {
-            Self::Add => left + right,
-            Self::Sub => left - right,
-            Self::Mul => left * right,
-            Self::Div => left / right,
-            Self::Pow => left.powf(right),
-        }
-    }
-}
-
-#[derive(Copy, Clone, PartialEq)]
-pub enum Paren {
-    Left,
-    Right,
-} impl Paren {
-    pub fn from_char(c: char) -> Option<Self> {
-        match c {
-            '(' => Some(Self::Left),
-            ')' => Some(Self::Right),
-            _ => None,
-        }
-    }
-
-    pub fn to_char(&self) -> char {
-        match self {
-            Self::Left => '(',
-            Self::Right => ')',
-        }
-    }
-}
-
-struct Node {
+#[derive(Clone, Copy)]
+struct ArrayNode {
     token: Token,
     left: Option<usize>,
     right: Option<usize>,
-} impl Node {
+} impl ArrayNode {
     fn new(token: Token) -> Self {
-        Node {
+        ArrayNode {
             token,
             left: None,
             right: None,
         }
     }
 
-    fn evaluate(&self, nodes: &Vec<Node>) -> f64 {
+    fn evaluate(&self, nodes: &Vec<ArrayNode>) -> f64 {
         match self.token {
             Token::Operator(op) => {
                 let l = if let Some(left) = self.left {
@@ -96,7 +38,7 @@ struct Node {
             Token::Paren(_) => panic!("this shouldnt happen")
         }
     }
-} impl std::fmt::Debug for Node {
+} impl std::fmt::Debug for ArrayNode {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self.token {
             Token::Operator(op) => {
@@ -114,11 +56,11 @@ struct Node {
     }
 }
 
-struct Tree {
-    nodes: Vec<Node>, /* root idx 0 */
-} impl Tree {
+struct ArrayTree {
+    nodes: Vec<ArrayNode>, /* root idx 0 */
+} impl ArrayTree {
     fn new() -> Self {
-        Tree { nodes: Vec::new() }
+        ArrayTree { nodes: Vec::new() }
     }
 
     fn evaluate(&self) -> f64 {
@@ -135,50 +77,156 @@ macro_rules! timeit {
 
         $b
 
-        let end = start.elapsed();
-        eprintln!("Time taken: {:?} ms", end.as_millis());
+        eprintln!("Time taken: {:?} ns", start.elapsed().as_nanos());
     }}
 }
 
 struct LinkedNode<'a> {
     token: Token,
-    left: Option<&'a Node>,
-    right: Option<&'a Node>,
+    left: Option<&'a LinkedNode<'a>>,
+    right: Option<&'a LinkedNode<'a>>,
+} impl<'a> LinkedNode<'a> {
+    fn new(token: Token) -> Self {
+        LinkedNode {
+            token,
+            left: None,
+            right: None,
+        }
+    }
+    
+    fn evaluate(&self) -> f64 {
+        match self.token {
+            Token::Operator(op) => {
+                let l = if let Some(left) = self.left {
+                    left.evaluate()
+                } else {
+                    0.0
+                };
+
+                let r = if let Some(right) = self.right {
+                    right.evaluate()
+                } else {
+                    0.0
+                };
+
+                op.evaluate(l, r)
+            },
+            Token::Value(v) => v,
+            Token::Paren(_) => panic!("this shouldnt happen")
+        }
+    }
+}
+
+struct LinkedTree<'a> {
+    root: LinkedNode<'a>,
+} impl<'a> LinkedTree<'a> {
+    fn new(root: LinkedNode<'a>) -> Self {
+        LinkedTree { root }
+    }
+
+    fn evaluate(&self) -> f64 {
+        self.root.evaluate()
+    }
 }
 
 #[test]
 fn test_evaluate() {
-    let mut t = Tree::new();
-    let mut root = Node::new(Token::new("+"));
-    let mut l = Node::new(Token::new("+"));
-    let mut ll = Node::new(Token::new("-"));
-    let mut llr = Node::new(Token::new("10"));
-    let mut lr = Node::new(Token::new("*"));
-    let mut lrl = Node::new(Token::new("2"));
-    let mut lrr = Node::new(Token::new("10"));
-    let mut r = Node::new(Token::new("/"));
-    let mut rl = Node::new(Token::new("-"));
-    let mut rlr = Node::new(Token::new("10"));
-    let mut rr = Node::new(Token::new("*"));
-    let mut rrl = Node::new(Token::new("10"));
-    let mut rrr = Node::new(Token::new("11"));
+    // Test equation: -10+(2*10)+(-10)+(10*11)
+    // Expected result: 110
+    eprint!("\nManual allocation of centralized array tree: ");
+    let mut t    = ArrayTree::new();
+    let mut root = ArrayNode::new(Token::new("+"));
+    let mut l    = ArrayNode::new(Token::new("+"));
+    let mut ll   = ArrayNode::new(Token::new("-"));
+    let mut llr  = ArrayNode::new(Token::new("10"));
+    let mut lr   = ArrayNode::new(Token::new("*"));
+    let mut lrl  = ArrayNode::new(Token::new("2"));
+    let mut lrr  = ArrayNode::new(Token::new("10"));
+    let mut r    = ArrayNode::new(Token::new("+"));
+    let mut rl   = ArrayNode::new(Token::new("-"));
+    let mut rlr  = ArrayNode::new(Token::new("10"));
+    let mut rr   = ArrayNode::new(Token::new("*"));
+    let mut rrl  = ArrayNode::new(Token::new("10"));
+    let mut rrr  = ArrayNode::new(Token::new("11"));
+    
+    rr.left = Some(11);
+    rr.right = Some(12);
+    rl.right = Some(9);
+    r.left = Some(8);
+    r.right = Some(10);
+    root.right = Some(7);
+    lr.left = Some(5);
+    lr.right = Some(6);
+    ll.right = Some(3);
+    l.right = Some(4);
+    l.left = Some(2);
+    root.left = Some(1);
+
     t.nodes = vec![
-        root,   // +
-        l,      // +
-        ll,     // -
-        llr,    // 10
-        lr,     // *
-        lrl,    // 2
-        lrr,    // 10
-        r,      // /
-        rl,     // -
-        rlr,    // 10
-        rr,     // *
-        rrl,    // 10
-        rrr,    // 11
+        root,   // +    0
+        l,      // +    1
+        ll,     // -    2
+        llr,    // 10   3
+        lr,     // *    4
+        lrl,    // 2    5
+        lrr,    // 10   6
+        r,      // +    7   
+        rl,     // -    8
+        rlr,    // 10   9
+        rr,     // *    10
+        rrl,    // 10   11
+        rrr,    // 11   12
     ];
+    eprintln!("Done.");
+
     eprint!("Recursive centralized array test: ");
     timeit!({
-        
-    })
+        eprintln!("{}", t.evaluate());
+    });
+
+
+    eprint!("\nLinked tree initialization: ");
+    let mut root = LinkedNode::new(Token::new("+"));
+    let mut l = LinkedNode::new(Token::new("+"));
+    let mut ll = LinkedNode::new(Token::new("-"));
+    let mut llr = LinkedNode::new(Token::new("10"));
+    let mut lr = LinkedNode::new(Token::new("*"));
+    let mut lrl = LinkedNode::new(Token::new("2"));
+    let mut lrr = LinkedNode::new(Token::new("10"));
+    let mut r = LinkedNode::new(Token::new("+"));
+    let mut rl = LinkedNode::new(Token::new("-"));
+    let mut rlr = LinkedNode::new(Token::new("10"));
+    let mut rr = LinkedNode::new(Token::new("*"));
+    let mut rrl = LinkedNode::new(Token::new("10"));
+    let mut rrr = LinkedNode::new(Token::new("11"));
+    
+    rr.left = Some(&rrl);
+    rr.right = Some(&rrr);
+
+    rl.right = Some(&rlr);
+
+    r.left = Some(&rl);
+    r.right = Some(&rr);
+
+    root.right = Some(&r);
+
+    lr.left = Some(&lrl);
+    lr.right = Some(&lrr);
+
+    ll.right = Some(&llr);
+    l.left = Some(&ll);
+    l.right = Some(&lr);
+    root.left = Some(&l);
+
+    let mut t = LinkedTree::new(root);
+
+    eprintln!("Done.");
+
+    eprint!("Linked test: ");
+
+    timeit!({
+        eprintln!("{}", t.evaluate());
+    });
+
+    eprintln!("");
 }
